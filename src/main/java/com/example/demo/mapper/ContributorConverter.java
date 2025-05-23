6577a6aa264592309452696b7032ca33c9a860a0
mahajjouji <mahajjouji@sqli.com>
2025-05-23 15:16:39 +0100

    update converter

--- src/main/java/com/example/demo/mapper/ContributorConverter.java
+++ src/main/java/com/example/demo/mapper/ContributorConverter.java
@@ -1,5 +1,25 @@
 package com.example.demo.mapper;
 
+import com.example.demo.dto.ContributorDto;
+import com.example.demo.entities.Contributors;
+
 public class ContributorConverter {
+    public static ContributorDto toDto(Contributors contributor) {
+        if (contributor == null) {
+            return null;
+        }
+        return new ContributorDto(contributor.getId(), contributor.getName(), contributor.getEmail());
+    }
+
+    public static Contributors toEntity(ContributorDto dto) {
+        if (dto == null) {
+            return null;
+        }
+        Contributors contributor = new Contributors();
+        contributor.setId(dto.id());
+        contributor.setName(dto.name());
+        contributor.setEmail(dto.email());
+        return contributor;
+    }
 
 }
