5e3829b1ebda65433cb685f29bc7d584d0d1dd58
mahajjouji <mahajjouji@sqli.com>
2025-05-23 16:27:35 +0100

    add swagger

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,10 +1,13 @@
 package com.example.demo;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(title = "Demo API", version = "1.0", description = "Demo API Documentation"))
 public class DemoApplication {
 
     public static void main(String[] args) {
--- src/main/java/com/example/demo/controllers/ContributorController.java
+++ src/main/java/com/example/demo/controllers/ContributorController.java
@@ -4,6 +4,7 @@ import com.example.demo.dto.ContributorDto;
 import com.example.demo.entities.Contributors;
 import com.example.demo.mapper.ContributorConverter;
 import com.example.demo.services.ContributorService;
+import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -23,18 +24,21 @@ public class ContributorController {
 
     // Example endpoint to create a new user
     @RequestMapping(value = "/contributor", method = RequestMethod.POST)
+    @Operation(summary = "Create a new contributor", description = "Create a new contributor with the provided details")
     public ResponseEntity<ContributorDto> createContributor(@RequestBody ContributorDto contributors) {
         contributorService.createContributor(ContributorConverter.toEntity(contributors));
         return ResponseEntity.status(HttpStatus.CREATED).body(contributors);
     }
 
     @RequestMapping(value = "/contributor/{cId}", method = RequestMethod.GET)
+    @Operation(summary = "Get contributor by ID", description = "Retrieve a contributor by their ID")
     public ResponseEntity<ContributorDto> getContributorById(@PathVariable Long cId) {
         ContributorDto contributor = ContributorConverter.toDto(contributorService.getContributorById(cId));
         return ResponseEntity.status(HttpStatus.OK).body(contributor);
     }
 
     @RequestMapping(value = "/contributors", method = RequestMethod.GET)
+    @Operation(summary = "Get all contributors", description = "Retrieve a list of all contributors")
     public ResponseEntity<List<ContributorDto>> getAll() {
         List<ContributorDto> contributors = ContributorConverter.toDtoList(contributorService.getAll());
         if (contributors.isEmpty()) {
@@ -44,6 +48,7 @@ public class ContributorController {
     }
 
     @RequestMapping(value = "/contributor/{cId}", method = RequestMethod.PUT)
+    @Operation(summary = "Update contributor", description = "Update the details of an existing contributor")
     public ResponseEntity<ContributorDto> updateContributor(@PathVariable Long cId, @RequestBody ContributorDto data) {
         ContributorDto contributor = ContributorConverter.toDto(contributorService.updateContributor(cId, ContributorConverter.toEntity(data)));
         if (contributor == null) {
